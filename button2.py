1657bb5a245bdc578b1f2403c89236b31d3e82c0
huilinghe19 <huiling.he@helmholtz-berlin.de>
2021-08-17 16:34:47 +0200

    seperate parameters in GUI

--- button2.py
+++ button2.py
@@ -23,15 +23,11 @@ def getResult(widget):
     return text
 
 testdevice1 = ophyd.Device(name="det")
-#det_name = getResult(w1)
 keithley6517_current = EpicsSignal("EMILEL:test:rdCurE3", name="keithley6517_current", labels=("detectors"))
 geek_list = ["Select one...", testdevice1.name, keithley6517_current.name]
 
 m1 = EpicsMotor('IOCsim:m1', name="m1", labels=("motors",))
 app = QApplication(["Scan App Version 1"])
-
-
-
 
 def plan():
     yield from scan([det], motor, -1, 1, 4)
@@ -39,11 +35,7 @@ def plan():
 def plan1():
     #yield from scan([keithley6517_current], m1_sim, -10, 10, 10)
     yield from scan([keithley6517_current], motor, -10, 10, 10)
-    
-def plan_setting(devicename):
-    
-    print(device)
-    yield from scan([device], num=10)
+
 
 def getPlanEditor():
     plan_editor = PlanEditorXafs(model)
@@ -55,9 +47,7 @@ class Widget(QWidget):
 
         self.button = QPushButton("Scan")
         self.button.clicked.connect(self.on_clicled)   
-       
-        #lable = QLabel()
-        #lable.setText("TutorialsPoint")
+
         
         self.lineEdit = QLineEdit()
         self.current_text = None                                                         # +++
@@ -123,50 +113,35 @@ class Widget(QWidget):
     # Just for this example, generate some data before starting this app.
     # Again, in practice, this should happen in a separate process and send
     # the results over a network.   
-        
-       
-       
-        
-        #
+
         if self.current_text == "det":
             print("det is chosen, scan det")
             self.lineEdit.setText("scan det, please wait..") 
             RE(plan())
-            
-            
+
         elif self.current_text == "keithley6517_current":
             print("det1 is chosen, scan keithley7517 current")
             self.lineEdit.setText("scan keithley6517 current, please wait...")
-          
-            
-            #plan_editor = PlanEditorXafs(model)
-            #self.h_box.addWidget(plan_editor, stretch=1)
+
             RE(plan1())
-            
-        
+              
         else:
             det_name = getResult(self.w1)
             mot_name = getResult(self.w2)
             start = getResult(self.w3)
             stop =   getResult(self.w4)
             number = getResult(self.w5)
-               
-       
-            
-
+   
             def plan_custom():
                 device_custom = ophyd.Signal(name=det_name)
-                
                 motor_custom = EpicsMotor('IOCsim:m1', name=mot_name, labels=("motors",))
                 start = getResult(self.w3)
                 stop =   getResult(self.w4)
                 number = getResult(self.w5)
-               
                 yield from scan([device_custom], motor_custom, float(start), float(stop), int(number))
-            #
+
             RE(plan_custom())
-               
-            # 
+
             #self.lineEdit.setText("scan {}, please wait...".format(self.detectors))
         self.h_box.addWidget(view)
             
